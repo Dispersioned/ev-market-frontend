8cffede02a761f45564df308e4eaad18502e30b7
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-03 21:16:29 +0300

    fix: reactnode child

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -10,8 +10,8 @@ export function Layout({ children }: LayoutProps) {
   return (
     <UILayout>
       <Header />
-      <Container maxWidth="xl" component="main">
-        {children}
+      <Container maxWidth="xl">
+        <main>{children}</main>
       </Container>
       <Footer />
     </UILayout>
