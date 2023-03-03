0e98b0483efae759bfe667dec8e62a2b3e0cead6
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-03 20:21:08 +0300

    Feature/insights on home page (#3)
    * ref: fonts
    
    * feat: create hero

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,22 @@
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
 import { Layout } from 'widgets/layout';
 
+import { Hero } from './styles';
+
 export function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <Hero>
+        <Typography variant="h1" fontSize={78} fontWeight={400}>
+          Electric Vehicle market
+        </Typography>
+        <Typography variant="h4">First in Rostov-on-Don</Typography>
+      </Hero>
+      <Link to={ROUTES.catalog}>
+        <Button variant="contained">Check the catalog</Button>
+      </Link>
+    </Layout>
+  );
 }
