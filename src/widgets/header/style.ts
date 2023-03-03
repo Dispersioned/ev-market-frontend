d739780f676d7ea74b93a3ec3434d9a1ce9c95e8
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-03 20:01:46 +0300

    feat: add nav styling

--- src/widgets/header/style.ts
+++ src/widgets/header/style.ts
@@ -1,4 +1,5 @@
-import { styled } from '@mui/material';
+import { Typography, styled } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { COLORS } from 'shared/config/colors';
 
 export const UIHeader = styled('header')`
@@ -16,3 +17,35 @@ export const Content = styled('div')`
 export const NavList = styled('ul')`
   list-style-type: none;
 `;
+
+export const NavItem = styled(Typography)`
+  position: relative;
+  color: #fff;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 0;
+    height: 1px;
+    background-color: #fff;
+    transition: all 0.3s ease;
+  }
+
+  &::after {
+    top: -5px;
+  }
+
+  &::before {
+    bottom: -5px;
+  }
+
+  &:hover {
+    &::before,
+    &::after {
+      width: 100%;
+    }
+  }
+` as typeof Typography;
