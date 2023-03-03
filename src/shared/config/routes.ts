6f4550966cb21651bd1d1ff0e3878e940885b457
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-03 13:08:55 +0300

    ref: extract routes

---
+++ src/shared/config/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  admin: 'admin',
+  auth: 'auth',
+  cart: 'cart',
+  catalog: 'catalog',
+  checkout: 'checkout',
+  compare: 'compare',
+  home: '/',
+  order: 'order/:id',
+  profile: 'profile',
+} as const;
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@mui/material';
 import Logo from 'assets/icons/logo.svg';
 import { Link } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
 
 import { Content } from './style';
 
@@ -9,7 +10,7 @@ export function Header() {
     <header>
       <Container maxWidth="xl">
         <Content>
-          <Link to="/">
+          <Link to={ROUTES.home}>
             <img src={Logo} alt="logo" />
           </Link>
         </Content>
