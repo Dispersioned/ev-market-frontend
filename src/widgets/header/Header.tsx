60370b22260811298c6d3f5b369fcfd3b744ab62
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-03 13:04:35 +0300

    feat: header logo

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,3 +1,19 @@
+import { Container } from '@mui/material';
+import Logo from 'assets/icons/logo.svg';
+import { Link } from 'react-router-dom';
+
+import { Content } from './style';
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Container maxWidth="xl">
+        <Content>
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </Content>
+      </Container>
+    </header>
+  );
 }
