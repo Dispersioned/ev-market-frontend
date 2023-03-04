f93b59a7c31b85c3ce39ccf545b133d0b9cabd77
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-04 15:22:06 +0300

    feat: import type from backend

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -11,6 +11,45 @@ export type ILocation = {
   // more?
 };
 
+export type IManufacturer = {
+  id: number;
+  name: string;
+};
+
+// need more research
+export type IChargingOption = {
+  id: number;
+  description: string;
+};
+
+export type IDriveTrain = {
+  RWD: 'RWD';
+  FWD: 'FWD';
+  AWD: 'AWD';
+};
+
+export type ICarColor = {
+  undefined: undefined;
+};
+
+export type IBodyStyle = {
+  Sedan: 'Sedan';
+  Coupe: 'Coupe';
+  SportsCar: 'SportsCar';
+  StationWagon: 'StationWagon';
+  HatchBack: 'HatchBack';
+  Convertible: 'Convertible';
+  SUV: 'SUV';
+  MiniVan: 'MiniVan';
+  PickUpTruck: 'PickUpTruck';
+};
+
+export type IVehicleCondition = {
+  New: 'New';
+  PreOwned: 'PreOwned';
+  CertifiedPreOwned: 'CertifiedPreOwned';
+};
+
 export type ICarView = {
   id: number;
   name: string;
@@ -25,4 +64,43 @@ export type ICarView = {
   mileage?: number;
   // if owned
   location?: ILocation;
+};
+
+export type ICar = {
+  id: number;
+  // year + Manufacturer + model
+  carName: string;
+  manufacturer: IManufacturer;
+  // in $ ?
+  price: number;
+  // kw
+  power: number;
+  range: number;
+  // kwh
+  batteryCapacity: number;
+  chargingOptions: IChargingOption[];
+  driveTrain: IDriveTrain;
+  accelerationTime: number;
+  topSpeed: number;
+  weight: number;
+  frontCargoVolume: number;
+  rearCargoVolume: number;
+  seatingCapacity: number;
+  exteriorColor: ICarColor;
+  bodyStyle: IBodyStyle;
+  manufacturingDate: Date;
+
+  // for get by id
+  ImageUrls: string[];
+
+  // if used
+  mileage: number;
+  // if used
+  condition: IVehicleCondition;
+  // if used
+  location: ILocation;
+  // if used
+  listedForSaleDate: Date;
+  // if used
+  sellersStory: string;
 };
