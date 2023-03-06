7513adc29c968e05ac97fdd7786e9af2ceb45d5d
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-06 18:48:46 +0300

    feat: add login button to header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,13 +1,17 @@
 import { Container, Typography } from '@mui/material';
+import loginIcon from 'assets/icons/login.svg';
 import logoIcon from 'assets/icons/logo.svg';
 import profileIcon from 'assets/icons/profile.svg';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'shared/config/routes';
+import { useTypeSelector } from 'shared/hooks/redux';
 
 import { Navigation } from './Navigation';
 import { Content, UIHeader } from './styles';
 
 export function Header() {
+  const user = useTypeSelector((state) => state.viewer.user);
+
   return (
     <UIHeader>
       <Container maxWidth="xl">
@@ -17,9 +21,15 @@ export function Header() {
           </Link>
           <Navigation />
           <Typography>contact us</Typography>
-          <Link to={ROUTES.profile}>
-            <img src={profileIcon} alt="profile" />
-          </Link>
+          {user ? (
+            <Link to={ROUTES.profile}>
+              <img src={profileIcon} alt="profile" />
+            </Link>
+          ) : (
+            <Link to={ROUTES.login}>
+              <img src={loginIcon} alt="login" />
+            </Link>
+          )}
         </Content>
       </Container>
     </UIHeader>
