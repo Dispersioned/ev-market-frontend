71df246c5f01c4b3eade4de230b4c03ad29a11d2
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-06 19:05:12 +0300

    feat: create guarded routes

---
+++ src/app/router/RoleRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { ROUTES } from 'shared/config/routes';
+import { useTypeSelector } from 'shared/hooks/redux';
+import { IRole } from 'shared/types/user';
+
+type RoleRouteProps = {
+  roles?: IRole[];
+};
+
+export function RoleRoute({ roles }: RoleRouteProps) {
+  const user = useTypeSelector((state) => state.viewer.user);
+
+  if (roles && user && !roles.includes(user.role)) return <Navigate to={ROUTES.home} />;
+
+  return <Outlet />;
+}
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,1 +1,9 @@
+import { IRole } from './user';
+
 export * from './car';
+
+export type IRoute = {
+  path: string;
+  element: JSX.Element;
+  roles?: IRole[];
+};
