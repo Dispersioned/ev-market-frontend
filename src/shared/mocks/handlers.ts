3fe900d9a135f8e07c9a5f670d36d38f8c7033a0
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-08 20:32:36 +0300

    feat: create cart mocks

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -4,6 +4,7 @@ import { rest } from 'msw';
 
 import { carMocks } from './car';
 import { carsViewMock } from './carsView';
+import { cartMock } from './cart';
 import { userMock } from './user';
 
 export const handlers = [
@@ -45,5 +46,32 @@ export const handlers = [
   }),
   rest.post('http://localhost:5000/auth/me', (req, res, ctx) => {
     return res(ctx.json(userMock));
+  }),
+  rest.get('http://localhost:5000/cart', (req, res, ctx) => {
+    return res(ctx.json(cartMock));
+  }),
+  rest.post('http://localhost:5000/cart/add', (req, res, ctx) => {
+    return res(ctx.json({ message: 'Added to cart' }));
+  }),
+  rest.get('http://localhost:5000/cart/quantity/:carId', (req, res, ctx) => {
+    const carId = +req.params.carId;
+
+    const cartItem = cartMock.find((item) => item.car.id === carId);
+
+    if (!cartItem) {
+      return res((resp) => {
+        resp.body = JSON.stringify({
+          message: 'Lot not in cart',
+        });
+        resp.status = 404;
+        return resp;
+      });
+    }
+
+    return res(
+      ctx.json({
+        quantity: cartItem.quantity,
+      })
+    );
   }),
 ];
