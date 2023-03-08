b1c504a8c901b6b110b178da574af364b78c8ad7
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-08 21:00:17 +0300

    feat: basic cart page

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,3 +1,24 @@
+import { cartApi } from 'shared/api/cartApi';
+import { ErrorScreen } from 'shared/ui/error-screen';
+import { Loading } from 'shared/ui/loading';
+import { Layout } from 'widgets/layout';
+
 export function Cart() {
-  return <div>Cart</div>;
+  const { data: cart, isLoading, error } = cartApi.useFetchCartQuery();
+
+  if (isLoading)
+    return (
+      <Layout>
+        <Loading />
+      </Layout>
+    );
+
+  if (error)
+    return (
+      <Layout>
+        <ErrorScreen error={error} />
+      </Layout>
+    );
+
+  return <Layout>Cart</Layout>;
 }
