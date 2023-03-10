e74b0ed7fcf8c874779da6d1ea0826ea464aecdf
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-10 13:27:39 +0300

    fix: cart layout

--- src/pages/cart/Cart.styles.ts
+++ src/pages/cart/Cart.styles.ts
@@ -2,6 +2,7 @@ import { styled } from '@mui/material';
 
 export const CartLayout = styled('div')`
   display: grid;
-  grid-template-columns: 1fr fit-content(100%);
+  grid-template-columns: 1fr minmax(200px, 300px);
+  align-items: flex-start;
   gap: 20px;
 `;
