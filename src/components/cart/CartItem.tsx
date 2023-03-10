98d743f3064bb25c7f89038902b71d5ab0a580b6
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-03-10 13:06:58 +0300

    ref: extract boolean guard

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Typography } from '@mui/material';
+import { CarNameLink } from 'components/car/car-name-link';
 import { ChangeQuantity } from 'components/car/change-quantity';
 import { Flex, GridArea } from 'shared/styles';
 import { ICarView } from 'shared/types';
@@ -11,24 +12,26 @@ type CartItemProps = {
 };
 
 export function CartItem({ car, quantity }: CartItemProps) {
+  const isAvailable = car.availableQuantity !== 0;
+
   return (
     <LotLayout>
       <GridArea name="image">
         <LotImg src={car.imageUrl} alt={car.carName} />
       </GridArea>
       <GridArea name="name">
-        <Typography variant="h5">{car.carName}</Typography>
+        <CarNameLink carId={car.id} name={car.carName} />
       </GridArea>
       <GridArea name="quantity">
-        {car.availableQuantity === 0 ? (
+        {isAvailable ? (
+          <ChangeQuantity carId={car.id} quantity={quantity} availableQuantity={car.availableQuantity} />
+        ) : (
           <Flex col gap={10} alignItems="flex-end">
             <Typography variant="h6">Lot not available</Typography>
             <Button variant="contained" color="error">
               Remove from cart
             </Button>
           </Flex>
-        ) : (
-          <ChangeQuantity carId={car.id} quantity={quantity} availableQuantity={car.availableQuantity} />
         )}
       </GridArea>
     </LotLayout>
